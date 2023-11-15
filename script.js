9ff9e7e689c6faf76adf5f52d55cb5186f0559ce
abdulgonimurod1 <abdulgonimurod1@gmail.com>
2023-11-15 22:42:08 +0700

    Object create pada function declaration

--- script.js
+++ script.js
@@ -14,42 +14,51 @@
 
 // 2. Function Declaration
 
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+const methodMahasiswa = {
+    makan : function (porsi) {
+        this.energi += porsi;
+        console.log(`halo ${this.nama}, selamat makan`);
+    },
 
-//     mahasiswa.makan = function (porsi) {
-//         this.energi += porsi;
-//         console.log(`halo ${nama}, selamat makan`);
-//     }
+    main : function (jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat bermain`);
+    },
+    
+    tidur : function (jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur`);
+    }
+}
 
-//     mahasiswa.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${mahasiswa.nama}, selamat bermain`);
-//     }
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
 
-//     return mahasiswa;
-// }
+    
 
-// let goni = Mahasiswa('goni', 10)
+    return mahasiswa;
+}
+
+let goni = Mahasiswa('goni', 10);
 
 // 3. Constructor Function
 // keyword new
 
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
 
-    this.makan = function (porsi) {
-        this.energi += porsi;
-        console.log(`halo ${this.nama}, selamat makan`);
-    }
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`halo ${this.nama}, selamat makan`);
+//     }
 
-    this.main = function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain`);
-    }
-}
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain`);
+//     }
+// }
 
-let goni = new Mahasiswa('Abdul Goni', 20)
+// let goni = new Mahasiswa('Abdul Goni', 20)
