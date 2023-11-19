e1cdbd31f2b6ac95f9dd8dc680abe8b739e8796a
abdulgonimurod1 <abdulgonimurod1@gmail.com>
2023-11-19 21:42:32 +0700

    Closure

--- script2.js
+++ script2.js
@@ -27,17 +27,57 @@
 // argumen
 // hoisting
 
-function satu() {
-    var nama = "Abdul Goni";
-    console.log(nama);
-}
+// function satu() {
+//     var nama = "Abdul Goni";
+//     console.log(nama);
+// }
 
-function dua() {
-    console.log(nama);
-}
+// function dua() {
+//     console.log(nama);
+// }
 
-console.log(nama);
-var nama = "erik";
-satu();
-dua("Dodi");
-console.log(nama);
+// console.log(nama);
+// var nama = "erik";
+// satu();
+// dua("Dodi");
+// console.log(nama);
+
+// Lexical Scope
+
+// function init() {
+//     // let nama = "Abdul Goni Murod";
+//     return function(nama){
+//         console.log(nama);
+//     }
+//     // return tampilNama;
+// }
+// let panggilNama = init();
+// panggilNama('Abdul Goni Murod');
+// panggilNama('Abu Bakar');
+
+// Factory Function
+
+// function ucapkanSalam(waktu){
+//     return function(nama){
+//         console.log(`Halo ${nama}, Selamat ${waktu}, semoga hari kamu menyenangkan!`);
+//     }
+// }
+
+// let selamatPagi = ucapkanSalam('pagi');
+// let selamatSiang = ucapkanSalam('siang');
+// let selamatMalam = ucapkanSalam('malam');
+
+// selamatMalam('Abdul Goni');
+
+// Contoh Closure & Private Method
+
+let add = (function () {
+    let counter = 0;
+    return function() {
+        return counter++;
+    }
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
